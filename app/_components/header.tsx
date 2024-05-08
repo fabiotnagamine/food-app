e1c7109be16dd494320e0037a3ee27e2f0fee664
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-05-07 21:32:51 -0300

    feat: Add Search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 const Header = () => {
   return (
-    <div className="p flex justify-between px-5 pt-6">
+    <div className="p flex justify-between px-5 pt-6 ">
       <Image src="/logo.svg" alt="FoodApp" height={30} width={100} />
       <Button
         size="icon"
