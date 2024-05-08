ae567003e8b40dd1000126dedfbab4f77854fe38
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-05-07 22:02:31 -0300

    feat: Add search component

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -4,8 +4,8 @@ import { Input } from "./ui/input";
 
 const Search = () => {
     return ( 
-        <div className="flex gap-2 ">
-            <Input className="" />
+        <div className="flex gap-2 pt-5 px-5">
+            <Input placeholder="Search restaurants" className="border- bg-slate-200" />
             <Button size="icon">
                 <SearchIcon size={20} />
             </Button>
