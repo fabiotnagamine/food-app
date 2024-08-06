479eaf5ccac220a67ac381dbeed4ec2e49699849
fabiotnagamine <nagamine.fabio.1998@gmail.com]>
2024-08-05 22:00:43 -0300

    feat: Add PromoBanner component to homepage

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -4,23 +4,14 @@ import Image from "next/image";
 
 const CategoryList = async () => {
   const categories = await db.category.findMany({});
-  return (<>
-   <div className="grid grid-cols-2 space-y-2  ">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-    <div className="pt-6 px-5">
-    <Image src="/Banner.png" alt="Até 30 porcento"
-    width={0}
-    height={0}
-    className="w-full h-auto object-contain"
-    sizes="100vw"
-    quality={100}
-    />
-    </div>
-  </>
-   
+  return (
+    <>
+      <div className="grid grid-cols-2 space-y-2  ">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ProductList from "./_components/product-list";
 import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
 
 const Home = async () => {
   const product = await db.product.findMany({
@@ -14,33 +15,47 @@ const Home = async () => {
       },
     },
     take: 500,
-      include: {
-        restaurant: {
-          select: {
-            name: true,
-          },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
         },
-        }
-  }
-  );
+      },
+    },
+  });
   return (
     <>
       <Header />
       <div className="px-5 pt-6">
         <Search />
       </div>
+
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
-      <div className="pt-6 space-y-2">
+
+      <div className="px-5 pt-6">
+        <PromoBanner src="/Banner.png" alt="Até 30 porcento" />
+      </div>
+
+      <div className="space-y-2 pt-6">
         <div className="flex justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button variant="ghost" className="text-primary hover:bg-transparent h-fit">
+          <Button
+            variant="ghost"
+            className="h-fit text-primary hover:bg-transparent"
+          >
             Ver todos
-            <ChevronRightIcon size={16}/>
+            <ChevronRightIcon size={16} />
           </Button>
         </div>
         <ProductList products={product} />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/Banner_2.png"
+          alt="A partir de R$ 17,90 em lanches"
+        />
       </div>
     </>
   );
